d1c7e566453b922d221377d1f4631799adf43b61
Mainul Hasan Khan <ahnafkhanhibban@outlook.com>
2025-04-08 22:07:23 +0600

    day-01

---
+++ day-01/script.js
@@ -0,0 +1,5 @@
+console.log('Hello, tapaScript!');
+console.log('Welcome to 40 Days of JavaScript!');
+// document.write('Check the browser console for a message!');
+
+document.querySelector('#demo').innerHTML = 'Hello, World!'; // Change the HTML content of an element with id="demo"
